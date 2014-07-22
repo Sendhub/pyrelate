77b8ad0703166e51821e952bfe761ac5dc040e27
Ben Chan <benchan@sendhub.com>
2014-07-21 17:37:54 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='pyrelate',
-    version='1.0.0',
+    version='1.0.1',
     author='sendwithus',
     author_email='matt@sendwithus.com',
     packages=find_packages(),
